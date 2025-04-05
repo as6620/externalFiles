53599ec12aa2f663e7da0eac3c53c8f0c6e12e05
as6620 <as6620@bs.amalnet.k12.il>
2025-04-05 13:02:41 +0300

    xml + credits

--- app/src/main/java/com/example/externalfiles/MainActivity.java
+++ app/src/main/java/com/example/externalfiles/MainActivity.java
@@ -1,19 +1,63 @@
 package com.example.externalfiles;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tV;
+    EditText eT;
+    String textEt;
+    private final String FILENAME = "inttest.txt";
+    String textFile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tV = (TextView) findViewById(R.id.tV);
+        eT = (EditText) findViewById(R.id.eT);
+
+
+    }
+
+
+    /**
+     * Inflates the options menu.
+     *
+     * @param menu The options menu.
+     * @return true if the menu is created successfully.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /**
+     * Handles selection of menu items.
+     *
+     * @param item The selected menu item.
+     * @return true if the menu item was handled successfully.
+     */
+    @Override
+    public boolean onOptionsItemSelected(@Nullable MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.menuCred) {
+            Intent si = new Intent(this, activity_credits.class);
+            startActivity(si);
+        }
+        return true;
     }
 }
