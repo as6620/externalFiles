a56878f9ca5a0cac982c446baeee689e484f5977
as6620 <as6620@bs.amalnet.k12.il>
2025-04-05 13:55:02 +0300

    onClicks

--- app/src/main/java/com/example/externalfiles/MainActivity.java
+++ app/src/main/java/com/example/externalfiles/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -21,8 +22,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -63,26 +67,76 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION);
     }
 
-    public void goSave(View view) {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission to access external storage granted", Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(this, "Permission to access external storage NOT granted", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void goSave(View view) {
+        try {
+            File externalDir = Environment.getExternalStorageDirectory();
+            File file = new File(externalDir, FILENAME);
+            file.getParentFile().mkdirs();
+            FileWriter writer = new FileWriter(file, true);
+            writer.write(eT.getText().toString());
+            writer.close();
+            tV.setText(getTextFile());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public String getTextFile() {
-
+        String text = "";
+        try {
+            File externalDir = Environment.getExternalStorageDirectory();
+            File file = new File(externalDir, FILENAME);
+            file.getParentFile().mkdirs();
+            FileReader reader = new FileReader(file);
+            BufferedReader bR = new BufferedReader(reader);
+            StringBuilder sB = new StringBuilder();
+            String line = bR.readLine();
+            while (line != null) {
+                sB.append(line+'\n');
+                line = bR.readLine();
+            }
+            bR.close();
+            reader.close();
+            text = sB.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return text;
     }
 
 
     public void goReset(View view) {
-
+        try {
+            File externalDir = Environment.getExternalStorageDirectory();
+            File file = new File(externalDir, FILENAME);
+            file.getParentFile().mkdirs();
+            FileWriter writer = new FileWriter(file);
+            writer.write("");
+            writer.close();
+            eT.setText("");
+            tV.setText("");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
 
     public void goExit(View view) {
         goSave(view);
         finish();
     }
-
 
 
     /**
